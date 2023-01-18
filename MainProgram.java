0fade48704238a0ace8fa9294d51c000bedffd78
Nisha <117558347+nishapoojary7@users.noreply.github.com>
2023-01-18 06:30:34 +0530

    Add files via upload

---
+++ MainProgram.java
@@ -0,0 +1,13 @@
+package abtractDemo;
+
+public class MainProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Teacher t1=new Teacher("abc","ajiet",1234,4,"professor","m.tech");
+		t1.biodata();
+		Student s1=new Student("def","ajiet40",45678,90,"good");
+		s1.biodata();
+
+	}
+}
